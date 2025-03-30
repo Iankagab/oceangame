ab341b63f720a5baa640e21edd24402ee5023773
iankag <ianka.grdn@gmail.com>
2025-03-30 12:02:54 -0300

    labels do nivel

--- game_code/Level.py
+++ game_code/Level.py
@@ -2,7 +2,10 @@
 #-*- coding: utf-8 -*-
 
 import pygame
-from Const import WIN_WIDHT, WIN_HEIGHT
+import sys
+from pygame import Surface, Rect
+from pygame.font import Font
+from Const import WIN_WIDHT, WIN_HEIGHT, COLOR_WHITE
 from Entity import Entity
 from EntityFactory import EntityFactory
 
@@ -14,10 +17,13 @@ class Level:
         self.entity_list: list[Entity]= []
 
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000
 
     def run (self,):
+        clock = pygame.time.Clock()
         running = True
         while running:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:  
                     running = False
@@ -26,7 +32,19 @@ class Level:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
 
-            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                     pygame.quit()
 
-        pygame.quit()
+            self.level_text(text_size=14, text=f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', text_color=COLOR_WHITE, text_pos=(10, 5))
+            self.level_text(text_size=14, text=f'fps: {clock.get_fps():.0f}', text_color=COLOR_WHITE, text_pos=(10, WIN_HEIGHT - 35))
+            self.level_text(text_size=14, text=f'entidades: {len(self.entity_list)}', text_color=COLOR_WHITE, text_pos=(10, WIN_HEIGHT - 20))
+            pygame.display.flip()
+        pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf = text_font.render(text, True, text_color) 
+        text_rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
 
